e22e9e83496c83fbd92bb71e4371f5225e1702b1
Douglas Moro <douglas.moro@unochapeco.edu.br>
2022-07-04 22:47:35 -0300

    Criação das funções de buscas.

--- curso/src/main/java/com/example/curso/resources/UserResources.java
+++ curso/src/main/java/com/example/curso/resources/UserResources.java
@@ -1,21 +1,37 @@
 package com.example.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.entities.User;
+import com.example.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-	
+
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		
-		User u = new User(1L, "Teste", "teste@gmail.com", "049988925503", "Tete123"); 
-		
-		return ResponseEntity.ok().body(u); 
-	}	
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) throws Exception {
+		User user = service.FindById(id);
+
+		if (user == null)
+			throw new Exception("Exception message");
+
+		return ResponseEntity.ok().body(user);
+	}
 }
