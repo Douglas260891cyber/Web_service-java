bc7a0293c744b5343ea1dad855cacf92c2558aa6
Douglas Moro <douglas.moro@unochapeco.edu.br>
2022-07-05 21:03:12 -0300

    Criação da tabela Order

--- curso/src/main/java/com/example/curso/entities/User.java
+++ curso/src/main/java/com/example/curso/entities/User.java
@@ -1,31 +1,36 @@
 package com.example.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable{
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; 
-	private String name; 
+	private Long id;
+	private String name;
 	private String email;
-	private String phone; 
-	private String password; 
-	
+	private String phone;
+	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -38,6 +43,10 @@ public class User implements Serializable{
 
 	public Long getId() {
 		return id;
+	}
+	
+	public List<Order> orders() {
+		return orders;
 	}
 
 	public void setId(Long id) {
@@ -92,6 +101,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-		
 
 }
